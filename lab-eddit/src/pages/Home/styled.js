835837a731f469db1f67a1261a03e4ac1ae0f02b
Joely Brito <joely_brito@outlook.com>
2022-07-22 20:20:48 -0300

    Adicionado pasta Home

---
+++ lab-eddit/src/pages/Home/styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+height: 100vh;
+padding: 0 32px;
+display: flex;
+align-items: center;
+flex-direction: column;
+justify-content: center;
+
+> p {
+    margin-bottom: 106px;
+}
+`
+
+export const Logo = styled.img`
+margin-bottom: 8px;
+`
+export const Form = styled.form`
+margin-bottom: 56px;
+`
+
+export const Input = styled.input`
+width: 362px;
+height: 60px;
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+border-radius: 5px;
+margin-bottom: 8px;
+border: none;
+padding-left: 16px;
+`
+
+export const Button = styled.button`
+width: 100%;
+height: 51px;
+background: ${props => props.outline ? "#FFFFFF" : "linear-gradient(90deg, #FF6489 0%, #F9B24E 100%)"};
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 27px;
+border:  ${props => props.outline ? " 1px solid #FE7E02" : "none"};
+font-weight: bold;
+color: ${props => props.outline ? "#FE7E02" : "#FFFFFF"};
+font-size: 18px;
+line-height: 25px;
+`
+
+export const Line = styled.hr`
+width: 100%;
+border: none;
+border: 1px solid #FE7E02;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+transform: rotate(0.32deg);
+margin: 18px 0;
+`
