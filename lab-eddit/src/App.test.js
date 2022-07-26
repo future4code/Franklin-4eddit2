d15e86d6285a4ca11e2bf15ca990c863f1a138e6
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-07-26 11:38:42 -0300

    Cria teste para a página de login

--- lab-eddit/src/App.test.js
+++ lab-eddit/src/App.test.js
@@ -1,7 +1,27 @@
 import React from "react";
 import App from "./App";
-import { render } from "../src/test-utils/customRender.js"
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+// import { render } from "../src/test-utils/customRender.js"
 
-test("Teste 1 - renderiza app", () => {
-  render(<App />)
+describe("Na página de login", () => {
+  test("o input nome é renderizado", () => {
+  const { getByPlaceholderText } = render(<App />)
+  const nome = getByPlaceholderText(/nome/i)
+  expect(nome).toBeInTheDocument()
+
+})
+test("o input senha é renderizado", () => {
+  const { getByPlaceholderText } = render(<App />)
+  const senha = getByPlaceholderText(/senha/i)
+  expect(senha).toBeInTheDocument()
+
+})
+test.skip("o botão 'Crie uma conta' leva para a página de cadastro", async () => {
+  const mockNavegacao = jest.fn()
+  const { getByText } = render(<App />)
+  const criarConta = getByText(/crie uma conta/i)
+  await userEvent.click(criarConta)
+  expect(mockNavegacao).toHaveBeenCalled(1)
+})
 });
