d19c1910bbfdf39546244f8cfad7062229101910
germannafol_linux <germannafol@outlook.com>
2022-07-29 17:29:39 -0300

    [feat] atulização cadastre e rotas

--- lab-eddit/src/routes/Router.js
+++ lab-eddit/src/routes/Router.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import FeedComments from '../pages/FeedComments/FeedComments';
 import FeedPage from '../pages/FeedPage/FeedPage';
 import Home from '../pages/Home/Home';
-import Cadastre from '../pages/Home/Cadastre'
+import Cadastre from '../pages/FeedCadastre/Cadastre'
 
 
 const Router = () => {
