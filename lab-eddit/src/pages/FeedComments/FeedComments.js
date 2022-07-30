11c7862abf2816fb864363a786d2564566957192
Joely Brito <joely_brito@outlook.com>
2022-07-30 19:44:30 -0300

    Adicionado hook de proteção de página

--- lab-eddit/src/pages/FeedComments/FeedComments.js
+++ lab-eddit/src/pages/FeedComments/FeedComments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BASE_URL } from "../../constants/url";
 import useRequestData from "../../hooks/useRequestData";
 import { BoxStyle } from "../../components/PostCard/styled";
@@ -7,62 +7,65 @@ import CommentCard from "../../components/CommentCard/CommentCard";
 import InputCard from "../../components/InputCard/InputCard";
 import useForm from "../../hooks/useForm";
 import axios from "axios";
+import useProtected from "../../hooks/useProtected";
 
 const FeedComments = () => {
+  useProtected();
+  const params = useParams();
+  // console.log(params)
 
-    const params = useParams()
-    // console.log(params)
+  const [form, handleInputChange, clear] = useForm({ body: "" });
 
-    const [form, handleInputChange, clear] = useForm({ body: ""})
+  const comments = useRequestData(
+    [],
+    `${BASE_URL}/posts/${params.id}/comments`
+  );
+  // console.log(comments)
 
-    const comments = useRequestData([], `${BASE_URL}/posts/${params.id}/comments`)
-    // console.log(comments)
+  const createComment = () => {
+    axios
+      .post(`${BASE_URL}/posts/${params.id}/comments`, form, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        alert(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    createComment();
+  };
 
-
-    const createComment = () => {
-        axios.post(`${BASE_URL}/posts/${params.id}/comments`, form, {
-            headers: {
-                Authorization: localStorage.getItem('token')
-            }
-        }).then((response) => {
-            alert(response.data)
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-
-    const onSubmitForm = (event) => {
-        event.preventDefault()
-        createComment()
-    }
-    
-    const postComments = comments.map((comment) => {
-        return(
-            <CommentCard
-                key={comment.id}
-                username={comment.username}
-                body={comment.body}
-                voteSum={comment.voteSum}
-            />
-        )
-    })
-
+  const postComments = comments.map((comment) => {
     return (
-        <form onSubmit={onSubmitForm}>
-            <h1>FeedComment</h1>
-            <InputCard
-                name={"body"}
-                value={form.body}
-                onChange={handleInputChange}
-                placeholder={"Adicionar comentário..."}
-                acao={"Responder"}
-            />
-            {postComments}
-        </form>
+      <CommentCard
+        key={comment.id}
+        username={comment.username}
+        body={comment.body}
+        voteSum={comment.voteSum}
+      />
+    );
+  });
 
-        
-    )
-}
+  return (
+    <form onSubmit={onSubmitForm}>
+      <h1>FeedComment</h1>
+      <InputCard
+        name={"body"}
+        value={form.body}
+        onChange={handleInputChange}
+        placeholder={"Adicionar comentário..."}
+        acao={"Responder"}
+      />
+      {postComments}
+    </form>
+  );
+};
 
 export default FeedComments;
